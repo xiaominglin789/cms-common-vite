8e34b9763519e0f3386e123aa7dbd0f02db8e92f
xiaominglin789 <xiaominglin789@gmail.com>
2021-12-21 17:17:02 +0800

    feat: 引入element-plus ui库

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,20 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
-function x() {
+function x () {
   console.log(123)
   const a = '2'
 }
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img
+    alt="Vue logo"
+    src="./assets/logo.png"
+  >
+  <el-button type="success">
+    element-plus
+  </el-button>
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
